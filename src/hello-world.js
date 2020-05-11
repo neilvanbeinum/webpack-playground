1bebef97ee93d7add3a6b19e11ebd70d36195b60
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-05-11 09:46:33 +0100

    Add bootstrap

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,5 +1,7 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js'
 import Heading from './components/heading/heading.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const heading = new Heading();
 heading.render('hello world');
